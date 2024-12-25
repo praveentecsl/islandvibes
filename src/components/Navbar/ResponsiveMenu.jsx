8a585356cdfe0d88e4579f30882c1b13c46c5519
praveentecsl <praveentecsl@gmail.com>
2024-12-25 11:00:50 +0530

    create the responsible features and add a video to home page

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const ResponsiveMenu = () => {
+const NavbarLinks = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Blogs",
+    link: "/blogs",
+  },
+  {
+    name: "Best Places",
+    link: "/best-places",
+  },
+]
+
+const ResponsiveMenu = ({showMenu,setShowMenu}) => {
   return (
-    <div>
-      ResponsiveMenu
+    <div
+      className={`${
+        showMenu ? "left-0" : "-left-[100%]"
+      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
+    >
+      <div className='Navbar_card'>
+        {/* top section */}
+        <div>
+        <div className="flex items-center justify-start gap-3">
+          <FaUserCircle size={50} />
+          <div>
+            <h1>Hello User</h1>
+            <h1 className="text-sm text-slate-500">Premium user</h1>
+          </div>
+        </div>
+        </div>
+        {/* Navlink Section */}
+        <div >
+        <ul className="space-y-4 text-xl">
+            {NavbarLinks.map((data) => (
+              <li>
+                <Link
+                  to={data.link}
+                  onClick={() => setShowMenu(false)}
+                  className="mb-5 inline-block"
+                >
+                  {data.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Mainvid from '../assets/video/Ho.mp4'
+
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <div>
+        <div className='relative h-[700px] '>
+          <video 
+            autoPlay
+            loop
+            muted
+            className='absolute right-0 top-0 h-[700px] w-full object-cover z-[-1] '
+          >
+            <source src= {Mainvid} 
+            type='video/mp4'/>
+          </video>
+        </div>
+      </div>
+      
+    </>
   )
 }
 
